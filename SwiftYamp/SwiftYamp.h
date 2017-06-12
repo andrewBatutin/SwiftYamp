67fe66fdda7b0573fef1bbbe0f2998660bd54068
Andrew Batutin <abatutin@gmail.com>
2017-06-12 13:52:31 +0300

    init commit

---
+++ SwiftYamp/SwiftYamp.h
@@ -0,0 +1,19 @@
+//
+//  SwiftYamp.h
+//  SwiftYamp
+//
+//  Created by Andrey Batutin on 6/12/17.
+//  Copyright © 2017 Andrey Batutin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftYamp.
+FOUNDATION_EXPORT double SwiftYampVersionNumber;
+
+//! Project version string for SwiftYamp.
+FOUNDATION_EXPORT const unsigned char SwiftYampVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftYamp/PublicHeader.h>
+
+
